772a94aadb94dd35365a1d7441f9389d241f9257
siqb <iqbals84@gmail.com>
2018-10-13 13:01:47 -0700

    Don't blindy symlink config files without giving a chance to review existing ones.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-ln -s ~/.vim/.vimrc ~/.vimrc
-ln -s ~/.vim/terminator-config ~/.config/terminator/config
+(ls ~/.vimrc && echo "vimrc file already exists! Please review it.") || (echo "Symlinking .vimrc file...." && ln -s ~/.vim/.vimrc ~/.vimrc)  
+(ls ~/.config/terminator/config && echo "Terminator config file already exists! Please review it.") || (echo "Symlinking .vimrc file...." && ln -s ~/.vim/terminator-config ~/.config/terminator/config)  
